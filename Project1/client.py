c373f8ef7fa35d00ae803f1d9130ebb8c4eda7a3
Chinmay Garg <chinmaygarg@me.com>
2020-01-10 17:35:35 -0800

    [working] added simple balance check from clients to server

--- Project1/client.py
+++ Project1/client.py
@@ -1,8 +1,10 @@
 import socket
 import os
 import json
+import pickle
 import argparse
 from linkedlist import Node, LinkedList
+from queue import PriorityQueue
 
 # check for valid command line arguments
 parser = argparse.ArgumentParser(description='Blockchain Client')
@@ -13,9 +15,12 @@ args = parser.parse_args()
 PID = os.getpid()
 HOSTNAME = socket.gethostname()
 PORT = args.port
+HEADERSIZE = 2
 
 # local queue for Lamport's Distributed Solution
-local_queue = LinkedList()
+# this list needs to be maintained based on the priority
+# TODO: maybe this needs to be a priority queue
+local_queue = PriorityQueue()
 
 # client debug information
 print(f"The client ID is {PID}\n")
@@ -23,11 +28,14 @@ print(f"Socket connection is running on port {PORT}")
 
 
 c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# c.bind((HOSTNAME, PORT))
+c.connect((HOSTNAME, 1234))
 lamport_clock = 0 # init clock
 
 while True:
     print("What type of transaction do you want to issue?\n1. Transfer\n2. Balance")
-    if 1:
+    option = int(input())
+    if option == 1:
         print("Enter the Reciever ID: ")
         reciever = input()
         print("Enter the amount you wish to send: ")
@@ -37,3 +45,17 @@ while True:
     else:
         print(f"Checking balance for {PID}...")
         # TODO: finish the balance logic
+        msg = pickle.dumps(PID)
+        print(pickle.loads(msg))
+        msg = bytes(f"{2:<{HEADERSIZE}}", "utf-8")+msg
+        print(msg)
+        c.send(msg)
+        current_balance = (c.recv(1024))
+        print(f"The client {PID} has the current balance of ${current_balance.decode()}.\n")
+
+
+def recv_msg(sock):
+    while True:
+        data, addr = c.recv(1024)
+        sys.stdout.write(data)
+
--- Project1/server.py
+++ Project1/server.py
@@ -1,9 +1,11 @@
 import socket
+import pickle
 from linkedlist import LinkedList, Node
 
 # hostname and port config
 HOST = socket.gethostname() # could just be 127.0.0.1
 PORT = 1234
+HEADERSIZE = 2
 
 # other configurations for clients
 INIT_BAL = 10
@@ -26,4 +28,16 @@ while True:
             # if there is no more data then break
             if not data:
                 break
+            else:
+                msg = (data[:HEADERSIZE])
+                msg = int(msg.decode())
+                print(f"The transaction type is {msg}.\n")
+                if msg == 1:
+                    print(f"Sending $x from a to b")
+                elif msg == 2:
+                    client_id = int(pickle.loads(data[HEADERSIZE:]))
+                    print(f"Checking the balance for client {client_id}")
+                    current_bal = bchain.calculateBalance(INIT_BAL, client_id)
+                    print(f"The current balance for {client_id} is {current_bal}\n")
+                    conn_sock.sendall(bytes(f"{current_bal}", "utf-8"))
 
